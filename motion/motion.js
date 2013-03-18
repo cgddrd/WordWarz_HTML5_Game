a5aeab5f1a96594bed039c91ea9e9b1d0a51c7d6
Connor GOddard <clg11@aber.ac.uk>
2013-03-18 15:58:49 +0000

    Updates angle drawing algorithm

--- motion/motion.js
+++ motion/motion.js
@@ -6,8 +6,8 @@ var CANVAS_HEIGHT = canvas.height;
 
 var FPS = 30;
 
-var textX = canvas.width / 2;
-var textY = 100;
+var textX = (canvas.width / 2);
+var textY = 50;
 
 var dx = 2;
 var dy = 4;
@@ -92,9 +92,9 @@ function Enemy(I) {
  // console.log("helo");
   
   var x1 = I.x;
-  var x2 = textX;
+  var x2 = textX + 16;
   var y1 = I.y;
-  var y2 = textY;
+  var y2 = textY + 16;
   
 var dx = Math.abs(x2 - x1);
 var dy = Math.abs(y2 - y1);
@@ -106,7 +106,7 @@ var err = dx - dy;
 
 while (true) {
 
-   // console.log(x1 + " - " + y1);
+    console.log(x1 + " - " + y1);
     I.storeCoords(x1, y1)
 
     if (x1 == x2 && y1 == y2) {
@@ -149,9 +149,9 @@ while (true) {
    
   // var targetAngle = (Math.atan2(textY - this.y, textX - this.x) * (180 / Math.PI))+90;
   
-  var targetAngle = (Math.atan2(textY - this.y, textX - this.x));
+  var targetAngle = (Math.atan2((textY + 16) - this.y, (textX + 16) - this.x));
    
-   //targetAngle = (targetAngle+360)%360;  
+   targetAngle = (targetAngle+360)%360;  
    
     if(this.angle != targetAngle) {
     
@@ -172,7 +172,7 @@ while (true) {
    context.save()
     
      //Set the origin to the center of the image
-    context.translate(this.x + this.width / 2, this.y + this.height / 2);
+    context.translate(this.x, this.y);
 
     //Rotate the canvas around the origin
    // context.rotate(rad);
@@ -180,13 +180,18 @@ while (true) {
    context.rotate(this.angle);
 
     //draw the image    
-    context.drawImage(katy_img,this.width / 2 * (-1),this.height / 2 * (-1),this.width,this.height);
+    context.drawImage(katy_img,(this.width / 2 * (-1)) - this.width / 2,(this.height / 2 * (-1)) - this.height / 2,this.width,this.height);
 
     //reset the canvas  
   //  context.rotate(rad * ( -1 ) );
   //  context.translate((this.x + this.width / 2) * (-1), (this.y + this.height / 2) * (-1));
     
     context.restore();   
+    
+      context.beginPath();
+      context.moveTo(I.coords[0].x, I.coords[0].y);
+      context.lineTo(I.coords[I.coords.length - 1].x, I.coords[I.coords.length - 1].y);
+      context.stroke();
 
   };
 
@@ -288,7 +293,7 @@ function update() {
  //   enemies.push(Enemy());
  // }
   
-   if(enemies.length < 10) {
+   if(enemies.length < 1) {
     enemies.push(Enemy());
   }
   
